1c0fd6e0159c6946c3b2d30ac2ca783d351d530a
Tomas Heiskanen <37534415+tomasheiskanen@users.noreply.github.com>
2023-10-27 12:05:38 +0000

    move prompts to text files

--- tiktok_reporter_analysis/common.py
+++ tiktok_reporter_analysis/common.py
@@ -19,30 +19,12 @@ def set_backend():
 
 
 def multi_modal_analysis(selected_frames, frames_folder, results_path):
-    SYSTEM_PROMPT = [  # From https://huggingface.co/spaces/HuggingFaceM4/idefics_playground/blob/main/app_dialogue.py
-        """The following is a conversation between a highly knowledgeable and intelligent visual AI assistant, called Assistant, and a human user, called User. In the following interactions, User and Assistant will converse in natural language, and Assistant will do its best to answer User’s questions. Assistant has the ability to perceive images and reason about the content of visual inputs. Assistant was built to be respectful, polite and inclusive. It knows a lot, and always tells the truth. When prompted with an image, it does not make up facts.
-    The conversation begins:""",
-        """\nUser:""",
-        "https://i1.pickpik.com/photos/515/906/355/kittens-cats-pet-cute-preview.jpg",
-        "Describe this image.<end_of_utterance>",
-        """\nAssistant: Five kittens are standing together in the center of the photograph. From the left to right, there is one orange kitten, two kittens white and black stripes, and two brown kittens with an orange head. They are in bright green grass and it looks like they are moving forward.<end_of_utterance>""",
-        "\nUser:How about this image?",
-        "https://cdn.pixabay.com/photo/2017/09/25/13/12/puppy-2785074_1280.jpg",
-        "Can you describe it too?<end_of_utterance>",
-        """\nAssistant: A dog is lying on the floor, looking at the camera. It is looking directly at you. The dog has a white body and brown patches on its face and ears. Its eyes are dark. Its nose is black, and it has long, floppy ears, white paws, long fur, big eyes.<end_of_utterance>""",
-        "\nUser: What kind of breed is it?<end_of_utterance>",
-        """\nAssistant: It looks like a Cavalier King Charles Spaniel. These breeds are known for their gentle, affectionate nature. Their long ears and big, brown eyes give them a sweet, expressive face. Cavaliers are small dogs weighing between 13 and 20 pounds. They have a silky, medium-length coat that comes in various colors, including black and tan, tricolor, red and white, or black.<end_of_utterance>""",
-        "\nUser: What can you tell me about this breed of dogs?<end_of_utterance>",
-        """\nAssistant: One specific characteristic of the Cavalier King Charles Spaniel is its friendly and affectionate nature. This breed is known for being extremely sociable and forming strong bonds with their owners. They are often described as "velcro dogs" because they love to be close to their human companions, whether it's sitting on their laps, cuddling on the couch, or simply following them around the house.<end_of_utterance>""",
-        "\nUser: ghjkhjabnufs<end_of_utterance>",
-        """\nAssistant: That doesn’t seem to be a word. Could you ask me another way?<end_of_utterance>""",
-        "\nUser: Do you like Cavalier King Charles Spaniel?<end_of_utterance>",
-        """\nAssistant: I do not have personal opinions as I’m just a computer program. However, cavaliers are known for being excellent family pets due to their gentle and patient demeanor, making them great with children and other pets. Their affectionate nature also makes them ideal therapy dogs, as they can provide comfort and emotional support to those in need.<end_of_utterance>""",
-        "\nUser: How many dogs do you see in this image?",
-        "https://i.dailymail.co.uk/i/pix/2011/07/01/article-2010308-0CD22A8300000578-496_634x414.jpg",
-        "<end_of_utterance>",
-        """\nAssistant: There is no dogs in this image. The picture shows a tennis player jumping to volley the ball.<end_of_utterance>""",
-    ]
+    with open("./tiktok_reporter_analysis/prompts/idefics_system_prompt.txt", "r") as f:
+        SYSTEM_PROMPT = f.readlines()
+    SYSTEM_PROMPT[-1] = SYSTEM_PROMPT[-1][:-1]  # Remove EOF newline
+
+    with open("./tiktok_reporter_analysis/prompts/idefics_prompt.txt", "r") as f:
+        PROMPT = f.read()[:-1]
 
     device = set_backend()
 
@@ -53,7 +35,6 @@ def multi_modal_analysis(selected_frames, frames_folder, results_path):
     )
     processor = AutoProcessor.from_pretrained(checkpoint, cache_dir=cache_dir)
 
-    PROMPT = """These are a two frames from a tiktok video. Write a one paragraph description of the video based on the two frames. Don't talk about the frames separately but infer what is happening in the whole video based on the frames."""
     prompts = []
     for video in selected_frames.keys():
         current_frames = selected_frames[video]
