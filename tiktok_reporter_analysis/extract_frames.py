98f40e2b1cbd490a408bd74a8654fa8957308176
Jesse McCrosky <mccrosky@gmail.com>
2023-11-29 13:28:33 +0200

    Modify extract_frames to use common method.

--- tiktok_reporter_analysis/extract_frames.py
+++ tiktok_reporter_analysis/extract_frames.py
@@ -2,12 +2,14 @@ import logging
 import os
 import shutil
 
-import cv2
+from moviepy.editor import VideoFileClip
 
 logger = logging.getLogger(__name__)
 
 
 def extract_frames_from_video(video_path, output_folder):
+    from tiktok_reporter_analysis.common import extract_frames
+
     # Ensure output directory exists and is empty
     if os.path.exists(output_folder):
         confirm = input(f"Output directory {output_folder} already exists. Remove it? (y/n) ")
@@ -19,33 +21,15 @@ def extract_frames_from_video(video_path, output_folder):
     os.makedirs(output_folder, exist_ok=True)
 
     # Open the video file
-    cap = cv2.VideoCapture(video_path)
+    cap = VideoFileClip(video_path) 
 
-    if not cap.isOpened():
-        logger.error("Error: Couldn't open the video file.")
-        return
+    # Extract frames using the method from common.py
+    frames_dataframe = extract_frames(cap, all_frames=True)
 
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    logger.info(f"Total frames: {frame_count}")
+    # Save the frames as images
+    for index, row in frames_dataframe.iterrows():
+        frame_image = row['image']
+        frame_image.save(os.path.join(output_folder, f"frame_{index}.png"))
 
-    count = 0
-    frames_n_timestamps = []
-    while True:
-        ret, frame = cap.read()
-
-        # Break the loop if video is ended
-        if not ret:
-            break
-
-        # Save the current frame as an image
-        timestamp = cap.get(cv2.CAP_PROP_POS_MSEC)
-        frames_n_timestamps.append({"frame": count, "timestamp": timestamp})
-        frame_filename = os.path.join(output_folder, f"frame_{count:04}.jpg")
-        cv2.imwrite(frame_filename, frame)
-
-        count += 1
-        logger.info(f"Extracted frame {count} of {frame_count}")
-
-    cap.release()
     logger.info("Frames extraction completed.")
-    return frames_n_timestamps
+    return frames_dataframe
