7e84ec4652a6db40d24ad0c8e99e098c5737a736
Jesse McCrosky <mccrosky@gmail.com>
2023-11-29 14:15:51 +0200

    Move import out of function.

--- tiktok_reporter_analysis/extract_frames.py
+++ tiktok_reporter_analysis/extract_frames.py
@@ -4,12 +4,12 @@ import shutil
 
 from moviepy.editor import VideoFileClip
 
+from tiktok_reporter_analysis.common import extract_frames
+
 logger = logging.getLogger(__name__)
 
 
 def extract_frames_from_video(video_path, output_folder):
-    from tiktok_reporter_analysis.common import extract_frames
-
     # Ensure output directory exists and is empty
     if os.path.exists(output_folder):
         confirm = input(f"Output directory {output_folder} already exists. Remove it? (y/n) ")
